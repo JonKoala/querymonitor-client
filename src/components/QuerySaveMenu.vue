866df2ff1e88c78eee5f6a987d4fcfe90fa42ec3
JonKoala <victor.santana.lemos@gmail.com>
2018-05-14 19:46:11 -0300

    adiciona menu para salvar query
    aplica o menu na view de sandbox

---
+++ src/components/QuerySaveMenu.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-card>
+    <v-toolbar color="blue-grey" dense card>
+      <v-toolbar-title class="white--text">{{ title }}</v-toolbar-title>
+    </v-toolbar>
+    <v-progress-linear v-bind:active="isLoading" class="my-0" color="blue" indeterminate></v-progress-linear>
+    <v-container fluid grid-list-md>
+      <v-layout row wrap>
+        <v-flex xs7>
+          <query-notepad v-model="value" readonly v-bind:style="{height: '200px'}"></query-notepad>
+        </v-flex>
+        <v-flex xs5>
+          <v-container fill-height class="pa-0">
+            <v-layout column>
+              <v-flex xs10>
+                <v-text-field v-model="titulo" v-bind:disabled="isLoading" label="Título" hide-details class="pb-4"></v-text-field>
+              </v-flex>
+              <v-flex xs2 class="pb-0">
+                <v-btn v-on:click="save()" v-bind:disabled="!isSavable || isLoading" color="ma-0 blue white--text">
+                  <v-icon left color="white">save</v-icon>salvar
+                </v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+import ApiService from 'services/api.service'
+
+import QueryNotepad from 'components/QueryNotepad'
+
+export default {
+  name: 'QuerySaveMenu',
+  props: {
+    value: { type: String },
+    edit: { type: Boolean }
+  },
+  components: {
+    QueryNotepad
+  },
+  data() {
+    return {
+      titulo: null,
+      isLoading: false
+    };
+  },
+  computed: {
+    title() {
+      return (this.edit) ? 'EDITAR QUERY' : 'SALVAR QUERY';
+    },
+    isSavable() {
+      return Boolean(this.value) && Boolean(this.titulo);
+    }
+  },
+  methods: {
+    async save() {
+      this.isLoading = true;
+      this.$emit('loading', true);
+
+      try {
+        var saved = await ApiService.post('queries', { titulo: this.titulo, corpo: this.value });
+        this.$emit('success', saved);
+      } catch(err) {
+        this.$emit('error', err);
+      } finally {
+        this.isLoading = false;
+        this.$emit('loading', false);
+      }
+    }
+  }
+}
+</script>
--- src/views/ViewSandbox.vue
+++ src/views/ViewSandbox.vue
@@ -6,7 +6,7 @@
 
           <v-toolbar color="blue-grey" dense card>
 
-            <v-menu v-model="editor" transition="slide-y-transition" bottom offset-y
+            <v-menu v-model="editorMenu" transition="slide-y-transition" bottom offset-y
               v-bind:close-on-content-click="false"
               v-bind:nudge-width="400"
               v-bind:nudge-bottom="10"
@@ -17,11 +17,11 @@
                 </v-btn>
                 <span>Query</span>
               </v-tooltip>
-              <query-editor v-model="query" class="pa-0" v-bind:height="300"></query-editor>
+              <query-editor v-model="query" class="pa-0" height="300px"></query-editor>
             </v-menu>
 
             <v-tooltip top>
-              <v-btn slot="activator" v-on:click="test()" v-bind:disabled="!this.isTestable" icon class="ma-0">
+              <v-btn slot="activator" v-on:click="runQuery()" v-bind:disabled="!this.isRunnable" icon class="ma-0">
                 <v-icon color="white">cached</v-icon>
               </v-btn>
               <span>Testar</span>
@@ -29,57 +29,107 @@
 
             <v-toolbar-title class="white--text">SANDBOX</v-toolbar-title>
 
+           <v-spacer></v-spacer>
+
+           <v-tooltip top>
+             <v-btn slot="activator" v-on:click="saveMenu = true" v-bind:disabled="!this.isRunnable" icon class="ma-0">
+               <v-icon color="white">save</v-icon>
+             </v-btn>
+             <span>Salvar</span>
+           </v-tooltip>
+
           </v-toolbar>
 
           <results-table v-model="results" v-bind:isLoading="isLoading" v-bind:error="errorMsg" class="pa-0"></results-table>
 
         </v-card>
       </v-flex>
+
+      <v-dialog v-model="saveMenu" v-bind:persistent="saveMenuLoading" max-width="600px">
+        <query-save-menu v-model="query"
+          v-on:loading="onSaveLoading"
+          v-on:success="onSaveSuccess"
+          v-on:error="onSaveError">
+        </query-save-menu>
+      </v-dialog>
+
+      <v-snackbar v-model="notification" v-bind:timeout="3000" bottom right>{{ notificationText }}</v-snackbar>
+
     </v-layout>
   </v-container>
 </template>
 
 <script>
 import ApiService from 'services/api.service'
+import NotepadService from 'services/notepad.service'
 
 import QueryEditor from 'components/QueryEditor'
+import QuerySaveMenu from 'components/QuerySaveMenu'
 import ResultsTable from 'components/ResultsTable'
 
 export default {
   name: 'ViewCreate',
   components: {
     QueryEditor,
+    QuerySaveMenu,
     ResultsTable
   },
   data() {
     return {
+
+      // query & results
       query: null,
       results: [],
       errorMsg: null,
       isLoading: false,
 
-      editor: false
+      // menus
+      editorMenu: false,
+      saveMenu: false,
+      saveMenuLoading: false,
+
+      // notifications
+      notification: false,
+      notificationText: null
     };
   },
   mounted() {
-    window.setTimeout(() => { this.editor = true; }, 200);
+    window.setTimeout(() => { this.editorMenu = true; }, 200);
   },
   computed: {
-    isTestable() {
+    isRunnable() {
       return Boolean(this.query);
     }
   },
   methods: {
-    async test() {
+    async runQuery() {
       this.isLoading = true;
+
       try {
-        this.results = await ApiService.get(`select/${this.query}`);
+        var inlineQuery = NotepadService.inline(this.query);
+        this.results = await ApiService.get(`select/${inlineQuery}`);
         this.errorMsg = null;
       } catch(err) {
         this.errorMsg = err.response.data.message;
       } finally {
         this.isLoading = false;
       }
+    },
+
+    // saveMenu events
+    onSaveLoading(isLoading) {
+      this.saveMenuLoading = isLoading;
+    },
+    onSaveSuccess() {
+      this.notificationText = "Query salva com sucesso!";
+      this.saveMenu = false;
+      this.notification = true;
+    },
+    onSaveError(err) {
+      this.notificationText = "Ocorreu um erro ao tentar salvar a query...";
+      this.saveMenu = false;
+      this.notification = true;
+      console.log(err);
     }
   }
 }
