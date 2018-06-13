9f03258f5d1b790713008f916bb512345bfc9a34
JonKoala <victor.santana.lemos@gmail.com>
2018-06-13 14:17:06 -0300

    adiciona testes para a bateria da ViewResults

--- test/ViewResults.js
+++ test/ViewResults.js
@@ -1,17 +1,18 @@
 import { expect } from 'chai'
 import { mount, createLocalVue } from '@vue/test-utils'
-import { sync } from 'vuex-router-sync'
 import sinon from 'sinon'
 import Vuetify from 'vuetify'
 import Vuex from 'vuex'
 
 import ApiService from 'services/api.service'
-import router from '../src/router'
 import store from 'store'
 import ViewResults from 'views/ViewResults'
 
+import { module as routeStub  } from './utils/route.module.stub'
+import { SET_ROUTE_PATH, SET_ROUTE_PARAMS } from './utils/route.module.stub'
+import { waitWhile } from './utils/utils'
+
 const localVue = createLocalVue();
-localVue.use(router);
 localVue.use(Vuetify);
 localVue.use(Vuex);
 
@@ -20,33 +21,76 @@ console.warn = function() {};
 console.error = function() {};
 
 
-async function createWrapper() {
+function createWrapper() {
 
   // stubbing the router
-  store.registerModule('route', { state: { path: 'results', params: { id: 1 } } });
+  store.registerModule('route', routeStub);
+  store.commit(SET_ROUTE_PATH, '/results');
+  store.commit(SET_ROUTE_PARAMS, { id: 1 });
 
-  var wrapper = mount(ViewResults, { localVue, store });
-  await wrapper.vm.$nextTick();
-  return wrapper;
+  return mount(ViewResults, { localVue, store });;
 }
 
 
 describe('ViewResults.vue', function() {
 
-  it('Should start by searching for the query specified on the url', async function() {
-    var query = { id: 1, titulo: 'title', corpo: 'query' };
-    var select = [{ col1: 'col1', col2: 'col2' }];
-
-    var getStub = sinon.stub(ApiService, 'get');
-    getStub.withArgs(`queries/${query.id}`).returns(query);
+  var query, select, getStub;
+  beforeEach(function() {
+    query = { id: 1, titulo: 'title', corpo: 'corpo' };
+    select = [{ col1: 'col1', col2: 'col2' }];
+    getStub = sinon.stub(ApiService, 'get');
+    getStub.withArgs('queries/1').returns(query);
     getStub.withArgs(`select/${query.corpo}`).returns(select);
+  });
+  afterEach(function() {
+    getStub.restore();
+  });
 
-    var wrapper = await createWrapper();
+  it('Should start by requesting for the query specified on the url and its results', async function() {
+
+    var wrapper = createWrapper();
+    await waitWhile(wrapper, () => wrapper.vm.isLoading);
     expect(getStub.callCount).to.equal(2);
     expect(wrapper.vm.paramId).to.equal(query.id);
     expect(wrapper.vm.queryTitle).to.equal(query.titulo);
+    expect(wrapper.vm.selectResult).to.equal(select);
+  });
 
-    getStub.resetBehavior();
+  it('Should request for the new specified query and its results when the url changes', async function() {
+    getStub.withArgs('queries/2').returns(query);
+
+    var wrapper = createWrapper();
+    store.commit(SET_ROUTE_PARAMS, { id: 2 });
+    await waitWhile(wrapper, () => wrapper.vm.isLoading);
+    expect(getStub.callCount).to.equal(4);
+    expect(wrapper.vm.paramId).to.equal(2);
+    expect(wrapper.vm.queryTitle).to.equal(query.titulo);
+    expect(wrapper.vm.selectResult).to.equal(select);
+  });
+
+  it('Should render the query title', async function() {
+
+    var wrapper = createWrapper();
+    await waitWhile(wrapper, () => wrapper.vm.isLoading);
+    expect(wrapper.find('.toolbar__title').text()).to.equal(query.titulo);
+  });
+
+  it('Should render the query string on a dialog', async function() {
+
+    var wrapper = createWrapper();
+    await waitWhile(wrapper, () => wrapper.vm.isLoading);
+    expect(wrapper.find('textarea').element.value).to.equal(query.corpo);
+  });
+
+  it('Should display the query string dialog only when the \'query\' button is clicked', async function() {
+
+    var wrapper = createWrapper();
+    await waitWhile(wrapper, () => wrapper.vm.isLoading);
+    expect(wrapper.find('.dialog').element.style.display).to.equal('none');
+
+    wrapper.find('button').trigger('click');
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find('.dialog').element.style.display).to.not.equal('none');
   });
 
 });
